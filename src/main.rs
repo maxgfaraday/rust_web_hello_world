c5a49253e6f6ebd455bcf0c089a71456f1abd7df
Max G. Faraday <maxgfaraday@6thcolumn.org>
2022-09-25 18:59:28 +0200

    Passing through the database connection thru run
    Now run takes an additional parameter so that running takes the
    service address as well as the connection to the database.
    
    Note:
    The health_check has not been adjusted to call run with the additional
    database parameter. [FIX ME: later] ;-)

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use rust_web_hello_world::startup as s;
 use rust_web_hello_world::configuration as conf;
+use sqlx::{Connection,PgConnection};
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let configuration = conf::get_configuration().expect("Failed to read configuration");
+    let connection = PgConnection::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgress");
     let address = format!("{}:{}", configuration.service.host,configuration.service.port);
-    let listener = TcpListener::bind(address).expect("Failed to bind service to port");
-    s::run(listener)?.await
+    let listener = TcpListener::bind(address)?;
+    s::run(listener,connection)?.await
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,17 +1,20 @@
 use crate::routes as r;
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use sqlx::PgConnection;
 use std::net::TcpListener;
 
 /**
 Runs the HTTP Server of the API
  */
-pub fn run(listener: TcpListener) -> Result<Server,std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, connection: PgConnection) -> Result<Server,std::io::Error> {
+    let connection = web::Data::new(connection);
+    let server = HttpServer::new(move || {
         App::new()
             .route("/",              web::get().to(r::greet))
             .route("/health_check",  web::get().to(r::health_check))
             .route("/subscriptions", web::post().to(r::subscribe))
+            .app_data(connection.clone())
     })
         .listen(listener)?
         .run();
