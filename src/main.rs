f7cad2495dfde57d90e60fe2b585683dedc66f8f
Max G. Faraday <maxgfaraday@6thcolumn.org>
2022-09-27 18:07:51 +0200

    Refactor: log tracing withing main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
-use tracing::subscriber::set_global_default;
+use rust_web_hello_world::configuration as conf;
+use rust_web_hello_world::startup as s;
+
+use tracing::{Subscriber,subscriber::set_global_default};
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_log::LogTracer;
 use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
-use rust_web_hello_world::configuration as conf;
-use rust_web_hello_world::startup as s;
 use sqlx::PgPool;
 use std::net::TcpListener;
+
+pub fn get_subscriber(name: String, env_filter: String) -> impl Subscriber + Send + Sync {
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(env_filter));
+    let formatting_layer = BunyanFormattingLayer::new(name, std::io::stdout);
+    Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer)
+}
+
+pub fn init_subscriber(subcriber: impl Subscriber + Send + Sync){
+    LogTracer::init().expect("Failed to get Logger");
+    set_global_default(subcriber).expect("Failed to set subscriber");
+}
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
@@ -13,14 +28,7 @@ async fn main() -> std::io::Result<()> {
     let configuration = conf::get_configuration().expect("Failed to read configuration");
 
     //logging and tracing setup...
-    LogTracer::init().expect("Failed to set logger");
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new("rust_web_hello_world".into(), std::io::stdout);
-    let subcriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
-    set_global_default(subcriber).expect("Failed to set subscriber");
+    init_subscriber(get_subscriber("rust_web_hello_world".into(), "info".into()));
 
     //database connection setup...
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
