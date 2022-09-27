9100ae0a0d47da9957d9a104b300018fd083d78d
Max G. Faraday <maxgfaraday@6thcolumn.org>
2022-09-27 20:19:47 +0200

    Telemetry: Refactor code into telemetry module

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,9 @@
 use rust_web_hello_world::configuration as conf;
 use rust_web_hello_world::startup as s;
+use rust_web_hello_world::telemetry as t;
 
-use tracing::{Subscriber,subscriber::set_global_default};
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_log::LogTracer;
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 use sqlx::PgPool;
 use std::net::TcpListener;
-
-pub fn get_subscriber(name: String, env_filter: String) -> impl Subscriber + Send + Sync {
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(env_filter));
-    let formatting_layer = BunyanFormattingLayer::new(name, std::io::stdout);
-    Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer)
-}
-
-pub fn init_subscriber(subcriber: impl Subscriber + Send + Sync){
-    LogTracer::init().expect("Failed to get Logger");
-    set_global_default(subcriber).expect("Failed to set subscriber");
-}
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
@@ -28,7 +11,7 @@ async fn main() -> std::io::Result<()> {
     let configuration = conf::get_configuration().expect("Failed to read configuration");
 
     //logging and tracing setup...
-    init_subscriber(get_subscriber("rust_web_hello_world".into(), "info".into()));
+    t::init_subscriber(t::get_subscriber("rust_web_hello_world".into(), "info".into()));
 
     //database connection setup...
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
