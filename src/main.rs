cefdce9473c330b3932f85a47ce0693d28a4aac5
Max G. Faraday <maxgfaraday@6thcolumn.org>
2022-09-27 17:38:57 +0200

    More in depth with tracing... adds subscriber, formatter and registry, oh my!

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use env_logger::Env;
+use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_log::LogTracer;
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 use rust_web_hello_world::configuration as conf;
 use rust_web_hello_world::startup as s;
 use sqlx::PgPool;
@@ -6,12 +9,28 @@ use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    //configuration setup...
     let configuration = conf::get_configuration().expect("Failed to read configuration");
+
+    //logging and tracing setup...
+    LogTracer::init().expect("Failed to set logger");
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    let formatting_layer = BunyanFormattingLayer::new("rust_web_hello_world".into(), std::io::stdout);
+    let subcriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+    set_global_default(subcriber).expect("Failed to set subscriber");
+
+    //database connection setup...
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to Postgress");
+
+    //service setup...
     let address = format!("{}:{}", configuration.service.host,configuration.service.port);
     let listener = TcpListener::bind(address)?;
+
+    //kick everything off...
     s::run(listener,connection_pool)?.await
 }
