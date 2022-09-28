fc59041a8c11b7600df95a028cc95270369b5428
Max G. Faraday <maxgfaraday@6thcolumn.org>
2022-09-28 15:57:19 +0200

    Pass Actix Web's App a wrapping TracingLogger to span individual requests

--- src/startup.rs
+++ src/startup.rs
@@ -3,6 +3,7 @@ use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing_actix_web::TracingLogger;
 
 /**
 Runs the HTTP Server of the API
@@ -11,6 +12,7 @@ pub fn run(listener: TcpListener, connection_pool: PgPool) -> Result<Server,std:
     let connection_pool = web::Data::new(connection_pool);
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(TracingLogger::default())
             .route("/",              web::get().to(r::greet))
             .route("/health_check",  web::get().to(r::health_check))
             .route("/subscriptions", web::post().to(r::subscribe))
