73bcbb11007a69677eaf24093d2d55a6c33342bf
Max G. Faraday <maxgfaraday@6thcolumn.org>
2022-10-22 13:46:39 +0200

    Using Types to check validity of data
    Prior to this commit we used the string values that came from the form
    as information for processing the form.  We then inspected this and
    realized that there would have to be some more strict validation
    placed on the data coming from the form.  So we created two struct
    tuples that would wrap the name and email fields that we got from the
    form.  Both these structs that wrapped Strings would have a ::parse
    function that would parse the passed in data and return the struct
    tuple wrapped in a Result object.
    
    pub struct SubscriberName(String);
    pub fn parse(s: String) -> Result<SubscriberName, String>
    
    pub struct SubscriberEmail(String);
    pub fn parse(s: String) -> Result<SubscriberEmail, String>
    
    Then these values may be used to construct the combined wrapping
    form...
    
    use crate::domain::subscriber_email::SubscriberEmail;
    use crate::domain::subscriber_name::SubscriberName;
    
    pub struct NewSubscriber {
        pub email: SubscriberEmail,
        pub name: SubscriberName
    }
    
    And then this would be used to query the database.
    
    I am still working out the magic hand off between these types
    FormData and NewSubscriber.
    
    new unit tests have been written to test the parse methods.

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,13 +1,25 @@
 // The collection of subscription related functionality
+use crate::domain::{NewSubscriber,SubscriberEmail,SubscriberName};
 use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
+use std::convert::{TryFrom};
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
     email: String,
     name: String
+}
+
+impl TryFrom<FormData> for NewSubscriber {
+    type Error = String;
+
+    fn try_from(form: FormData) -> Result<Self, Self::Error> {
+        let email = SubscriberEmail::parse(form.email)?;
+        let name  = SubscriberName::parse(form.name)?;
+        Ok(Self {email, name})
+    }
 }
 
 #[tracing::instrument (
